244b00129b6fd262f9af6766274f80ff4201ecf6
Jason Tung <mickeu420@gmail.com>
2018-09-28 02:55:44 -0400

    i did like nothing

---
+++ cmake-build-debug/CMakeFiles/04str.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/04str.dir/string.c.o"
+  "04str.pdb"
+  "04str"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/04str.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ string.c
@@ -0,0 +1,16 @@
+//
+// Created by Jason Tung on 9/27/18.
+//
+
+#include <stdio.h>
+
+int mystrlen(char * str){
+    
+}
+
+
+int main(){
+
+    printf("hu");
+    return 0;
+}
